685082c5d60712fbf7573e41e605f5051220d0de
igortchatcho <gaelnoufele@gmail.com>
2022-09-11 23:55:38 -0500

    Update and rename Ec2.tf to .tf

---
+++ .tf
@@ -0,0 +1,18 @@
+provider "aws" {
+  version = "~> 4.0"
+  region  = "us-east-2"
+}
+
+resource "aws_s3_bucket" "b" {
+  bucket = "my-tf-test-bucket"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket_acl" "example" {
+  bucket = aws_s3_bucket.b.id
+  acl    = "private"
+}
