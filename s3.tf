9d4d2f1090485a2c6317c9da5af1dcc81d3e1308
igortchatcho <gaelnoufele@gmail.com>
2022-09-18 02:25:20 -0500

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,14 +1,12 @@
 provider "aws" {
-  version = "~> 4.0"
   region  = "us-east-2"
 }
 
 resource "aws_s3_bucket" "b" {
-  bucket = "my-tf-test-bucket"
+  bucket = "my-tf-test-bucket-igor"
 
   tags = {
     Name        = "My bucket"
-    Environment = "Dev"
   }
 }
 
